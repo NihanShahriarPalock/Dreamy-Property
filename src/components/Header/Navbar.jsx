5c317b2efdbc0f38cfaffc56f9c5bd8848be501e
NihanShahriarPalock <nihanshahriar000@gmail.com>
2024-04-18 19:24:00 +0600

    DropDown changed

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -50,7 +50,7 @@ const Navbar = () => {
           </div>
           <ul
             tabIndex={0}
-            className='menu menu-sm dropdown-content mt-3 z-50 p-2 shadow bg-white rounded-box w-52'>
+            className='menu menu-sm dropdown-content mt-3 z-50  shadow bg-blue-500 rounded-box w-52 text-black'>
             {allLink}
           </ul>
         </div>
